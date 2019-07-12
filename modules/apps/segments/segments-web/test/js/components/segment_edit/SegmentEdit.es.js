17ba7faec3a13c56dc16876eac2a75feb7e49f33
Andrés Fulla <andres.fulla@liferay.com>
2019-07-12 08:14:03 +0200

    LPS-97655 Testing cancel button

--- modules/apps/segments/segments-web/test/js/components/segment_edit/SegmentEdit.es.js
+++ modules/apps/segments/segments-web/test/js/components/segment_edit/SegmentEdit.es.js
@@ -15,7 +15,7 @@
 import 'jest-dom/extend-expect';
 import React from 'react';
 import SegmentEdit from 'components/segment_edit/SegmentEdit.es';
-import {cleanup, render} from 'react-testing-library';
+import {cleanup, render, fireEvent, wait} from 'react-testing-library';
 import {SOURCES} from 'utils/constants.es';
 
 const SOURCE_ICON_TESTID = 'source-icon';
@@ -181,5 +181,150 @@ describe('SegmentEdit', () => {
 		);
 
 		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it('redirects when cancelling without any edition', () => {
+		const redirect = '/test-url';
+		const mockConfirm = jest.fn();
+		const mockNavigate = jest.fn();
+		window.Liferay.Util.navigate = mockNavigate;
+		window.confirm = mockConfirm;
+
+		const contributors = [
+			{
+				conjunctionId: 'and',
+				conjunctionInputId: 'conjunction-input-1',
+				initialQuery: "(value eq 'value')",
+				inputId: 'input-id-for-backend-form',
+				propertyKey: 'first-test-values-group'
+			}
+		];
+		const hasUpdatePermission = true;
+
+		const propertyGroups = [
+			{
+				entityName: 'First Test Values Group',
+				name: 'First Test Values Group',
+				properties: [
+					{
+						label: 'Value',
+						name: 'value',
+						options: [],
+						selectEntity: null,
+						type: 'string'
+					}
+				],
+				propertyKey: 'first-test-values-group'
+			}
+		];
+
+		const {getByText} = render(
+			<SegmentEdit
+				availableLocales={{
+					en_US: ''
+				}}
+				contributors={contributors}
+				defaultLanguageId='en_US'
+				hasUpdatePermission={hasUpdatePermission}
+				initialSegmentName={{
+					en_US: 'Segment title'
+				}}
+				locale='en_US'
+				propertyGroups={propertyGroups}
+				redirect={redirect}
+				showInEditMode={true}
+			/>
+		);
+
+		const cancelButton = getByText('cancel');
+
+		expect(cancelButton).not.toBe(null);
+
+		fireEvent.click(cancelButton);
+
+		expect(mockNavigate).toHaveBeenCalledTimes(1);
+		expect(mockNavigate).toHaveBeenCalledWith(redirect);
+		expect(mockConfirm).toHaveBeenCalledTimes(0);
+	});
+
+	it('redirects when cancelling after title edition', done => {
+		const redirect = '/test-url';
+		const mockConfirm = jest.fn();
+		const mockNavigate = jest.fn();
+		window.Liferay.Util.navigate = mockNavigate;
+		window.confirm = mockConfirm;
+
+		const contributors = [
+			{
+				conjunctionId: 'and',
+				conjunctionInputId: 'conjunction-input-1',
+				initialQuery: "(value eq 'value')",
+				inputId: 'input-id-for-backend-form',
+				propertyKey: 'first-test-values-group'
+			}
+		];
+		const hasUpdatePermission = true;
+
+		const propertyGroups = [
+			{
+				entityName: 'First Test Values Group',
+				name: 'First Test Values Group',
+				properties: [
+					{
+						label: 'Value',
+						name: 'value',
+						options: [],
+						selectEntity: null,
+						type: 'string'
+					}
+				],
+				propertyKey: 'first-test-values-group'
+			}
+		];
+
+		const {getByTestId, getByText} = render(
+			<SegmentEdit
+				availableLocales={{
+					en_US: ''
+				}}
+				contributors={contributors}
+				defaultLanguageId='en_US'
+				hasUpdatePermission={hasUpdatePermission}
+				initialSegmentName={{
+					en_US: 'Segment title'
+				}}
+				locale='en_US'
+				propertyGroups={propertyGroups}
+				redirect={redirect}
+				showInEditMode={true}
+			/>
+		);
+
+		const localizedInput = getByTestId('localized-main-input');
+		const cancelButton = getByText('cancel');
+
+		fireEvent.change(localizedInput, {
+			target: {
+				value: 'A'
+			}
+		});
+
+		wait(() => expect(localizedInput.value).toBe('A'), {
+			timeout: 3000
+		}).then(function() {
+			expect(localizedInput.value).toBe('A');
+
+			expect(cancelButton).not.toBe(null);
+
+			fireEvent.click(cancelButton);
+
+			expect(mockNavigate).toHaveBeenCalledTimes(0);
+
+			expect(mockConfirm).toHaveBeenCalledTimes(1);
+			expect(mockConfirm).toHaveBeenCalledWith(
+				'criteria-cancel-confirmation-message'
+			);
+			done();
+		});
 	});
 });

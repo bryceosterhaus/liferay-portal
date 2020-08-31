aabcbdc6434e828f60058a056b9ac2933dece64f
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2020-08-31 16:09:24 -0700

    Sample to display billboard.js version

--- modules/apps/frontend-js/frontend-js-clay-sample-web/src/main/resources/META-INF/resources/js/App.js
+++ modules/apps/frontend-js/frontend-js-clay-sample-web/src/main/resources/META-INF/resources/js/App.js
@@ -13,11 +13,14 @@
  */
 
 import ClayAlert from '@clayui/alert';
+import ClayChart, {bb} from '@clayui/charts';
 import React from 'react';
 
 import '../css/main.scss';
 
 export default () => {
+	console.log('VERSION: ', bb.version);
+
 	return (
 		<div>
 			<ClayAlert title="Info">
@@ -25,7 +28,69 @@ export default () => {
 				whatever JS you want to App.js and redeploy.
 			</ClayAlert>
 
-			<div className="clay-test-class">This is where your code goes.</div>
+			<ClayChart
+				axis={{
+					x: {
+						type: 'timeseries',
+					},
+				}}
+				data={{
+					columns: [
+						[
+							'x',
+							'2018-01-01',
+							'2018-02-01',
+							'2018-03-01',
+							'2018-04-01',
+							'2018-05-01',
+							'2018-06-01',
+							'2018-07-01',
+							'2018-08-01',
+							'2018-09-01',
+							'2018-10-01',
+							'2018-11-01',
+							'2018-12-01',
+						],
+						[
+							'product1',
+							130,
+							340,
+							200,
+							100,
+							40,
+							300,
+							{high: 240, low: 140, mid: 180},
+							{high: 380, low: 300, mid: 350},
+							{high: 480, low: 320, mid: 400},
+							{high: 260, low: 100, mid: 200},
+							{high: 140, low: 100, mid: 120},
+							{high: 180, low: 80, mid: 100},
+						],
+						[
+							'product2',
+							210,
+							180,
+							30,
+							90,
+							40,
+							120,
+							{high: 260, low: 180, mid: 240},
+							{high: 460, low: 360, mid: 420},
+							{high: 180, low: 80, mid: 120},
+							{high: 120, low: 60, mid: 80},
+							{high: 80, low: 10, mid: 20},
+							{high: 100, low: 20, mid: 60},
+						],
+					],
+					type: 'predictive',
+					types: {
+						product1: 'area-line-range',
+						product2: 'area-spline-range',
+					},
+					x: 'x',
+				}}
+				predictionDate="2018-06-01"
+			/>
 		</div>
 	);
 };

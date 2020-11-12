4c1452b9f1c3dbacdf0714ca11697d3e3762e057
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2020-11-12 10:47:28 -0800

    demo for search param updating in react

--- modules/apps/frontend-js/frontend-js-clay-sample-web/src/main/resources/META-INF/resources/js/App.js
+++ modules/apps/frontend-js/frontend-js-clay-sample-web/src/main/resources/META-INF/resources/js/App.js
@@ -17,7 +17,58 @@ import React from 'react';
 
 import '../css/main.scss';
 
+// disregard this util, I'm sure you'll need something more robust to get search params
+const getSearchParam = (key) => {
+	const urlParams = new URLSearchParams(window.location.search);
+
+	return urlParams.get(key);
+};
+
 export default () => {
+	const [page, setPage] = React.useState(getSearchParam('page'));
+	const [delta, setDelta] = React.useState(getSearchParam('delta'));
+
+	const handlePage = () => {
+		const newPage = Number(page) + 1;
+
+		setPage(newPage);
+
+		const newPath =
+			window.location.pathname + `?page=${newPage}&delta=${delta}`;
+
+		window.history.pushState(
+			{senna: true, path: newPath},
+			document.title,
+			newPath
+		);
+	};
+
+	const handleDelta = () => {
+		const newDelta = Number(delta) + 1;
+
+		setDelta(newDelta);
+
+		const newPath =
+			window.location.pathname + `?page=${page}&delta=${newDelta}`;
+
+		window.history.pushState(
+			{senna: true, path: newPath},
+			document.title,
+			newPath
+		);
+	};
+
+	React.useEffect(() => {
+		const listener = () => {
+			setPage(getSearchParam('page'));
+			setDelta(getSearchParam('delta'));
+		};
+
+		window.addEventListener('popstate', listener);
+
+		return () => window.removeEventListener('popstate', listener);
+	}, []);
+
 	return (
 		<div>
 			<ClayAlert title="Info">
@@ -25,7 +76,8 @@ export default () => {
 				whatever JS you want to App.js and redeploy.
 			</ClayAlert>
 
-			<div className="clay-test-class">This is where your code goes.</div>
+			<button onClick={handlePage}>Update page to {page}</button>
+			<button onClick={handleDelta}>Update delta to {delta}</button>
 		</div>
 	);
 };

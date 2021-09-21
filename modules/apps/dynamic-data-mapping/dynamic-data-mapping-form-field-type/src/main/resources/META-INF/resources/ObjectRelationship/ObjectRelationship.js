edb68b50f7939703334766f689d479cc0d1d5ce2
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2021-09-21 16:37:27 -0700

    LPS-139246 - Remove unnecessary autocomplete component

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/ObjectRelationship/ObjectRelationship.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/ObjectRelationship/ObjectRelationship.js
@@ -12,99 +12,116 @@
  * details.
  */
 
-import {useIsMounted} from '@liferay/frontend-js-react-web';
+import {useResource} from '@clayui/data-provider';
 import {FieldBase} from 'dynamic-data-mapping-form-field-type/FieldBase/ReactFieldBase.es';
-import {Autocomplete as BaseAutocomplete} from 'frontend-js-components-web';
-import {debounce, fetch} from 'frontend-js-web';
-import React, {useState} from 'react';
+import {useDebounce} from '@clayui/shared';
+import React from 'react';
+import ClayAutocomplete from '@clayui/autocomplete';
+import ClayDropDown from '@clayui/drop-down';
+import {fetch} from 'frontend-js-web';
 
-function getData(apiUrl, inputValue, page, pageSize) {
-	const url = new URL(apiUrl, themeDisplay.getPortalURL());
+const LoadingWithDebounce = ({loading, networkStatus, render}) => {
+	const debouncedLoadingChange = useDebounce(loading, 500);
 
-	if (inputValue) {
-		url.searchParams.set('search', inputValue);
+	if (networkStatus === 1 || debouncedLoadingChange) {
+		return (
+			<ClayDropDown.Item className="disabled">
+				{Liferay.Language.get('loading')}
+			</ClayDropDown.Item>
+		);
 	}
 
-	if (page) {
-		url.searchParams.set('page', page);
-	}
-
-	if (pageSize) {
-		url.searchParams.set('pageSize', pageSize);
-	}
-
-	return fetch(url, {
-		headers: new Headers({
-			Accept: 'application/json',
-			'Accept-Language': themeDisplay.getBCP47LanguageId(),
-			'Content-Type': 'application/json',
-		}),
-	}).then((data) => data.json());
-}
+	return render;
+};
 
 export function ObjectRelationship({
 	apiURL,
-	initialLabel,
-	initialValue,
+	initialValue = '',
+	initialLabel = '',
 	inputName,
-	labelKey,
+	labelKey = 'label',
 	name,
-	value,
-	valueKey,
+	valueKey = 'value',
 	...otherProps
 }) {
-	const [inputValue, setInputValue] = useState(initialLabel || '');
-	const [selectedItem, setSelectedItem] = useState(initialValue || value);
-	const [items, setItems] = useState(null);
-	const [loading, setLoading] = useState(false);
-	const isMounted = useIsMounted();
+	const [selectedValue, setSelectedValue] = React.useState(initialValue);
+	const [inputValue, setInputValue] = React.useState(initialLabel);
+	const [networkStatus, setNetworkStatus] = React.useState(1);
 
-	const fetchData = debounce((query) => {
-		if (query && isMounted()) {
-			setLoading(true);
+	const {resource} = useResource({
+		fetch,
+		fetchPolicy: 'cache-first',
+		link: apiURL,
+		onNetworkStatusChange: setNetworkStatus,
+		variables: {
+			page: 1,
+			pageSize: 10,
+			search: inputValue,
+		},
+	});
 
-			getData(apiURL, query, 1, 10)
-				.then((jsonResponse) => {
-					setItems(jsonResponse.items);
-
-					setLoading(false);
-
-					if (!query) {
-						return;
-					}
-
-					const found = jsonResponse.items.find(
-						(item) => item[labelKey] === query
-					);
-
-					if (found) {
-						setSelectedItem(found);
-					}
-				})
-				.catch(() => {
-					setLoading(false);
-				});
-		}
-	}, 500);
+	const initialLoading = networkStatus === 1;
+	const loading = networkStatus < 4;
 
 	return (
 		<FieldBase name={name} {...otherProps}>
-			<BaseAutocomplete
-				inputName={inputName}
-				inputValue={inputValue}
-				items={items}
-				labelKey={labelKey}
-				loading={loading}
-				name={name}
-				onInputChange={(val) => {
-					setInputValue(val);
-					setSelectedItem(null);
-					fetchData(val);
-				}}
-				onSelectedItemChange={setSelectedItem}
-				selectedItem={selectedItem}
-				valueKey={valueKey}
+			<input
+				id={inputName}
+				name={inputName}
+				type="hidden"
+				value={selectedValue}
 			/>
+
+			<ClayAutocomplete>
+				<ClayAutocomplete.Input
+					onChange={(event) => {
+						setSelectedValue('');
+						setInputValue(event.target.value);
+					}}
+					placeholder={Liferay.Language.get('search')}
+					value={inputValue}
+				/>
+
+				<ClayAutocomplete.DropDown
+					active={
+						selectedValue
+							? false
+							: (!!resource && !!inputValue) || initialLoading
+					}
+				>
+					<ClayDropDown.ItemList>
+						<LoadingWithDebounce
+							loading={loading}
+							networkStatus={networkStatus}
+							render={
+								<>
+									{resource?.items?.length === 0 && (
+										<ClayDropDown.Item className="disabled">
+											{Liferay.Language.get(
+												'no-results-found'
+											)}
+										</ClayDropDown.Item>
+									)}
+									{resource?.items?.map((item) => (
+										<ClayAutocomplete.Item
+											key={item.id}
+											match={String(inputValue)}
+											onClick={() => {
+												setSelectedValue(
+													item[valueKey]
+												);
+												setInputValue(item[labelKey]);
+											}}
+											value={String(item[labelKey])}
+										/>
+									))}
+								</>
+							}
+						/>
+					</ClayDropDown.ItemList>
+				</ClayAutocomplete.DropDown>
+				{loading && <ClayAutocomplete.LoadingIndicator />}
+			</ClayAutocomplete>
 		</FieldBase>
 	);
 }

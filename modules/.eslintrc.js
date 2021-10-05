4adbe8445b65c906e6c3a6e92cd91269f45d46e0
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2021-10-05 10:32:12 -0700

    LPS-139948 - Enable eslint @liferay/aui/no-one rule

--- modules/.eslintrc.js
+++ modules/.eslintrc.js
@@ -45,7 +45,6 @@ config = {
 		'@liferay/aui/no-modal': 'off',
 		'@liferay/aui/no-node': 'off',
 		'@liferay/aui/no-object': 'off',
-		'@liferay/aui/no-one': 'off',
 		'@liferay/empty-line-between-elements': 'off',
 		'no-empty': ['error', {allowEmptyCatch: true}],
 		'notice/notice': [

100b4449a7a1be393817e250779c57c3624160b7
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2024-05-28 15:27:51 +0400

    LPD-24074 - Add format & lint tooling directly to liferay-portal

--- modules/.prettierrc.js
+++ modules/.prettierrc.js
@@ -1,10 +1,13 @@
-const liferayPlugin = require('@liferay/prettier-plugin');
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
 
 module.exports = {
 	bracketSpacing: false,
 	endOfLine: 'lf',
 	jsxSingleQuote: false,
-	plugins: [liferayPlugin],
+	plugins: ['@liferay/prettier-plugin'],
 	quoteProps: 'consistent',
 	singleQuote: true,
 	tabWidth: 4,

733d38a2e84b63edacabb951daeba0edef26db7d
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2024-10-24 09:45:44 +0400

    LPD-28139 React 18 updates for liferay-commerce

--- modules/apps/commerce/commerce-order-content-web/src/main/resources/META-INF/resources/js/multishipping/Multishipping.tsx
+++ modules/apps/commerce/commerce-order-content-web/src/main/resources/META-INF/resources/js/multishipping/Multishipping.tsx
@@ -259,7 +259,7 @@ const Multishipping = ({
 	}, [orderId, prepareData]);
 
 	const updateFullCart = useCallback(
-		async (data) => {
+		async (data: any) => {
 			setSaving(true);
 
 			await CommerceServiceProvider.DeliveryCartAPI('v1')
@@ -281,7 +281,7 @@ const Multishipping = ({
 	);
 
 	const handleDeleteDeliveryGroup = useCallback(
-		async (deliveryGroup) => {
+		async (deliveryGroup: any) => {
 			try {
 				if (deliveryGroups.length === 1) {
 					await updateFullCart(
@@ -323,14 +323,14 @@ const Multishipping = ({
 		[deliveryGroups, formattedOrderItems, updateFullCart]
 	);
 
-	const handlePaginationDeltaChange = useCallback((value) => {
+	const handlePaginationDeltaChange = useCallback((value: any) => {
 		setPagination((prevState) => ({
 			...prevState,
 			pageSize: value,
 		}));
 	}, []);
 
-	const handlePaginationPageChange = useCallback((value) => {
+	const handlePaginationPageChange = useCallback((value: any) => {
 		setPagination((prevState) => ({
 			...prevState,
 			currentPage: value,
--- workspaces/liferay-commerceminium4-workspace/client-extensions/liferay-commerceminium4-js-import-maps-entry/vite.config.js
+++ workspaces/liferay-commerceminium4-workspace/client-extensions/liferay-commerceminium4-js-import-maps-entry/vite.config.js
@@ -3,11 +3,12 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import react from '@vitejs/plugin-react';
+import {resolve} from 'path';
+
 /* eslint-disable no-undef */
 
 import {defineConfig} from 'vite';
-import {resolve} from 'path';
-import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 

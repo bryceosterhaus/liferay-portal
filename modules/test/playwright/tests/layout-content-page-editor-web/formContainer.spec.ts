3327eabf26d4a4727fa6135e459e9fb827fdb064
Victor Galan <victor.galan@liferay.com>
2024-10-29 15:57:19 +0100

    LPD-28139 Fix test

--- modules/test/playwright/tests/layout-content-page-editor-web/formContainer.spec.ts
+++ modules/test/playwright/tests/layout-content-page-editor-web/formContainer.spec.ts
@@ -631,6 +631,10 @@ test.describe('Text input field', () => {
 
 			await pageEditorPage.selectFragment(inputId);
 
+			await pageEditorPage.goToConfigurationTab('Styles');
+
+			await pageEditorPage.goToConfigurationTab('General');
+
 			const selectedOption = page
 				.getByLabel('Field', {exact: true})
 				.getByRole('option', {selected: true});
@@ -2287,6 +2291,10 @@ test.describe('Multistep', () => {
 			await page.locator('[data-multi-step-icon="2"]').click();
 
 			await pageEditorPage.selectFragment(textInputId);
+
+			await pageEditorPage.goToConfigurationTab('Styles');
+
+			await pageEditorPage.goToConfigurationTab('General');
 
 			await page.getByLabel('Field', {exact: true}).waitFor();
 

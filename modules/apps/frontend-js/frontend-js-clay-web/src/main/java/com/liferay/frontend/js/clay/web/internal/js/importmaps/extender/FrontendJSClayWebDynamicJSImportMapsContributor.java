02ecbb93a027de18a663ced26aaa16fb168d490c
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2025-12-19 11:52:59 +0400

    LPD-66847 - Add import maps

--- modules/apps/frontend-js/frontend-js-clay-web/src/main/java/com/liferay/frontend/js/clay/web/internal/js/importmaps/extender/FrontendJSClayWebDynamicJSImportMapsContributor.java
+++ modules/apps/frontend-js/frontend-js-clay-web/src/main/java/com/liferay/frontend/js/clay/web/internal/js/importmaps/extender/FrontendJSClayWebDynamicJSImportMapsContributor.java
@@ -3,14 +3,11 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.frontend.taglib.clay.internal.js.importmaps.extender;
+package com.liferay.frontend.js.clay.web.internal.js.importmaps.extender;
 
 import com.liferay.frontend.js.importmaps.extender.DynamicJSImportMapsContributor;
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.json.JSONException;
-import com.liferay.portal.kernel.json.JSONFactory;
-import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.url.builder.AbsolutePortalURLBuilder;
 import com.liferay.portal.url.builder.AbsolutePortalURLBuilderFactory;
@@ -19,21 +16,16 @@ import com.liferay.portal.url.builder.ESModuleAbsolutePortalURLBuilder;
 import jakarta.servlet.http.HttpServletRequest;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.Writer;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
- * @author Iván Zaera Avellón
+ * @author Bryce Osterhaus
  */
 @Component(service = DynamicJSImportMapsContributor.class)
-public class FrontendTaglibClayDynamicJSImportMapsContributor
+public class FrontendJSClayWebDynamicJSImportMapsContributor
 	implements DynamicJSImportMapsContributor {
 
 	@Override
@@ -47,7 +39,7 @@ public class FrontendTaglibClayDynamicJSImportMapsContributor
 			_absolutePortalURLBuilderFactory.getAbsolutePortalURLBuilder(
 				httpServletRequest);
 
-		for (String moduleName : _moduleNames) {
+		for (String moduleName : _MODULE_NAMES) {
 			if (!first) {
 				writer.write(", ");
 			}
@@ -64,7 +56,7 @@ public class FrontendTaglibClayDynamicJSImportMapsContributor
 
 			ESModuleAbsolutePortalURLBuilder esModuleAbsolutePortalURLBuilder =
 				absolutePortalURLBuilder.forESModule(
-					"frontend-taglib-clay",
+					"frontend-js-clay-web",
 					"exports/" + escapedModuleName + ".js");
 
 			writer.write(esModuleAbsolutePortalURLBuilder.build());
@@ -78,41 +70,52 @@ public class FrontendTaglibClayDynamicJSImportMapsContributor
 		HttpServletRequest httpServletRequest, Writer writer) {
 	}
 
-	@Activate
-	protected void activate() throws IOException, JSONException {
-		JSONObject packageJSONObject = _getPackageJSONObject();
-
-		JSONObject dependenciesJSONObject = packageJSONObject.getJSONObject(
-			"dependencies");
-
-		_moduleNames.clear();
-
-		for (String moduleName : dependenciesJSONObject.keySet()) {
-			if (!moduleName.startsWith("@clayui/")) {
-				continue;
-			}
-
-			_moduleNames.add(moduleName);
-		}
-	}
-
-	private JSONObject _getPackageJSONObject()
-		throws IOException, JSONException {
-
-		try (InputStream inputStream =
-				FrontendTaglibClayDynamicJSImportMapsContributor.class.
-					getResourceAsStream("dependencies/package.json")) {
-
-			return _jsonFactory.createJSONObject(StringUtil.read(inputStream));
-		}
-	}
+	private static final String[] _MODULE_NAMES = {
+		"@clayui/alert",
+		"@clayui/autocomplete",
+		"@clayui/badge",
+		"@clayui/breadcrumb",
+		"@clayui/button",
+		"@clayui/card",
+		"@clayui/charts",
+		"@clayui/color-picker",
+		"@clayui/core",
+		"@clayui/data-provider",
+		"@clayui/date-picker",
+		"@clayui/drop-down",
+		"@clayui/empty-state",
+		"@clayui/form",
+		"@clayui/icon",
+		"@clayui/label",
+		"@clayui/layout",
+		"@clayui/link",
+		"@clayui/list",
+		"@clayui/loading-indicator",
+		"@clayui/localized-input",
+		"@clayui/management-toolbar",
+		"@clayui/modal",
+		"@clayui/multi-select",
+		"@clayui/multi-step-nav",
+		"@clayui/nav",
+		"@clayui/navigation-bar",
+		"@clayui/pagination",
+		"@clayui/pagination-bar",
+		"@clayui/panel",
+		"@clayui/popover",
+		"@clayui/progress-bar",
+		"@clayui/provider",
+		"@clayui/shared",
+		"@clayui/slider",
+		"@clayui/sticker",
+		"@clayui/table",
+		"@clayui/tabs",
+		"@clayui/time-picker",
+		"@clayui/toolbar",
+		"@clayui/tooltip",
+		"@clayui/upper-toolbar",
+	};
 
 	@Reference
 	private AbsolutePortalURLBuilderFactory _absolutePortalURLBuilderFactory;
-
-	@Reference
-	private JSONFactory _jsonFactory;
-
-	private final List<String> _moduleNames = new ArrayList<>();
 
 }
